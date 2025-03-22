27d418bbf4e15e9b4918cd60d31f024bea1abe34
ksjba <hrhhdurj47@gmail.com>
2025-03-22 15:01:39 +0530

    Update golemxgroup.py

--- golemxgroup.py
+++ golemxgroup.py
@@ -161,7 +161,7 @@ def handle_attack(message):
 
     pending_feedback[user_id] = True  
 
-    full_command = f"./bgmi {target} {port} {time_duration} 900"
+    full_command = f"./bgmi {target} {port} {time_duration} 200"
 
     try:
         subprocess.run(full_command, shell=True, check=True)
